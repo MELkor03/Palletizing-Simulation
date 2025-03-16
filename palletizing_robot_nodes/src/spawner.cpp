6cc2af8484e485880ec1314ecae465ca54fb1be8
GrzegorzKozera <grzegorz.kozera100@gmail.com>
2025-03-16 18:09:52 +0100

    modified spawner node

--- palletizing_robot_nodes/src/spawner.cpp
+++ palletizing_robot_nodes/src/spawner.cpp
@@ -47,7 +47,7 @@ int main(int argc, char * argv[])
         request->initial_pose = pose;
 
         std::string box_id = "Box_" + std::to_string(i + 1);
-        collision_environment::addMoveitBox(planning_scene_interface, box_id, {pose.position.x, pose.position.y, pose.position.z}, {0.2, 0.3, 0.1});
+        collision_environment::addMoveitBox(planning_scene_interface, box_id, pose, {0.2, 0.3, 0.1});
 
         pose.position.x += 1;
         auto future = client->async_send_request(request);
@@ -57,7 +57,7 @@ int main(int argc, char * argv[])
             RCLCPP_ERROR(logger, "Faild to spawn object");
         }
         
-        std::this_thread::sleep_for(std::chrono::seconds(10));
+        if(i<1) { std::this_thread::sleep_for(std::chrono::seconds(10)); }
     }
 
     //collision_environment::addMoveitBox(planning_scene_interface, "Box_1", {1, 0, 0.65}, {0.2, 0.3, 0.1});
